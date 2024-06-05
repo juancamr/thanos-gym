2006c1d7a56fe45cf489dac7ab64085543596f18
Juan Carlos <jcmrojas29@gmail.com>
2024-06-04 22:33:18 -0500

    remove .settings in .gitignore

--- src/main/java/com/uni/thanosgym/Thanosgym.java
+++ src/main/java/com/uni/thanosgym/Thanosgym.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.uni.thanosgym;
 
 import com.uni.thanosgym.config.DbConnection;
 import com.uni.thanosgym.config.Startup;
 
-/**
- *
- * @author juancamr
- */
 public class Thanosgym {
 
     public static void main(String[] args) {
